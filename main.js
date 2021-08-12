5f40a2224326ac7a148136d7b14059963c317035
Avani-Shah <avani.shah@centurylink.com>
2021-08-12 23:07:12 +0000

    Lab 1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Update this constant with your ServiceNow credentials
 const options = {
-  url: 'yourDevServiceNowInstanceCanonicalHostname',
-  username: 'devUsername',
-  password: 'devUserPassword'
+  url: 'https://dev93608.service-now.com/',
+  username: 'admin',
+  password: 'zmCkMzRv7UE9'
 };
 
 
