91a538e12c7db2601125267389bf5fe1a15505e8
Avani-Shah <avani.shah@centurylink.com>
2021-08-19 19:06:07 +0000

    Lab 6

--- lab05.main.js
+++ lab05.main.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 /**
  * Import the ServiceNowConnector class from local Node.js module connector.js
- *   and assign it to constant ServiceNowConnector.git
+ *   and assign it to constant ServiceNowConnector.
  * When importing local modules, IAP requires an absolute file reference.
  * Built-in module path's join method constructs the absolute filename.
  */
@@ -84,61 +84,20 @@ class ServiceNowAdapter extends EventEmitter {
   }
 
   /**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
-healthcheck(callback) {
- this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-
-      this.emitOffline();
-      if (callback) {
-          callback(null, error);
-      }
-    
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-
-      this.emitOnline();
-      if (callback) {
-          callback(result, null);
-      }
-   }
- });
-}
+   * @memberof ServiceNowAdapter
+   * @method healthcheck
+   * @summary Check ServiceNow Health
+   * @description Verifies external system is available and healthy.
+   *   Calls method emitOnline if external system is available.
+   *
+   * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
+   *   that handles the response.
+   */
+  healthcheck(callback) {
+    // We will build this method in a later lab. For now, it will emulate
+    // a healthy integration by emmitting ONLINE.
+    this.emitOnline();
+  }
 
   /**
    * @memberof ServiceNowAdapter
@@ -159,7 +118,7 @@ healthcheck(callback) {
    * @description Emits an ONLINE event to IAP indicating external
    *   system is available.
    */
-    emitOnline() {
+  emitOnline() {
     this.emitStatus('ONLINE');
     log.info('ServiceNow: Instance is available.');
   }
@@ -173,7 +132,7 @@ healthcheck(callback) {
    *
    * @param {string} status - The event to emit.
    */
-    emitStatus(status) {
+  emitStatus(status) {
     this.emit(status, { id: this.id });
   }
 
@@ -191,11 +150,11 @@ healthcheck(callback) {
      * Write the body for this function.
      * The function is a wrapper for this.connector's get() method.
      * Note how the object was instantiated in the constructor().
-     * get() takes a callback function.
-     */
+     * get() takes a callback function.     */
 
         this.connector.get(callback);
-  }
+     
+     }
 
   /**
    * @memberof ServiceNowAdapter
